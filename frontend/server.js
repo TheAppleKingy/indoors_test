07a85393ac745b0d45815a00e92894b5e258745b
TheAppleKingy <pridanov.ali.job@gmail.com>
2026-05-06 14:35:33 +0700

    hotfix

--- frontend/server.js
+++ frontend/server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist/browser')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist/browser', 'index.html'));
 });
 
 const port = process.env.PORT || 8080;
